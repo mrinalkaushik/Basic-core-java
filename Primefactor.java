6c0fa106f358070c53b19168f2a60713fa11b422
mrinal kaushik <mrinalrocks24x7gmail.com>
2022-11-17 23:53:51 +0530

    ADD: prime factor of n number

---
+++ Primefactor.java
@@ -0,0 +1,23 @@
+package com.bridgelab.day5;
+
+import java.util.Scanner;
+
+public class Primefactor {
+
+	public static void main(String args[]) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a number ::");
+		int number = sc.nextInt();
+
+		for (int i = 2; i < number; i++) {
+			while (number % i == 0) {
+				System.out.println(i + " ");
+				number = number / i;
+			}
+		}
+		if (number > 2) {
+			System.out.println(number);
+		}
+	}
+}
